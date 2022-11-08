b501d3264f2ea3d81d246b64e749fee098cb71a7
xiao278 <xiao278@purdue.edu>
2022-11-08 16:08:14 -0500

    fixed multiplication crash bug

--- src/main/java/Multiplication.java
+++ src/main/java/Multiplication.java
@@ -4,15 +4,15 @@ import cc.redberry.rings.poly.multivar.MultivariatePolynomial;
 
 public class Multiplication {
 
-    public static Rational<MultivariatePolynomial<BigInteger>>[][] product(Matrix A, Matrix B) {
-        if (A.getCols() != B.getRows()) return null;
-        int inner = A.getCols();
-        Rational<MultivariatePolynomial<BigInteger>>[][] product = new Rational[A.getCols()][B.getCols()];
+    public static Rational<MultivariatePolynomial<BigInteger>>[][] product(Matrix left, Matrix right) {
+        if (left.getCols() != right.getRows()) return null;
+        int inner = left.getCols();
+        Rational<MultivariatePolynomial<BigInteger>>[][] product = new Rational[left.getRows()][right.getCols()];
         for (int i = 0; i < product.length; i++) {
             for (int j = 0; j < product[i].length; j++) {
                 Rational sum = Parser.parse("0");
                 for (int k = 0; k < inner; k++) {
-                    sum = sum.add(A.get(i, k).multiply(B.get(k, j)));
+                    sum = sum.add(left.get(i, k).multiply(right.get(k, j)));
                 }
                 product[i][j] = sum;
             }
